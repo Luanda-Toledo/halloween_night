92699f3c5e860f59aa5d54dcd3ee5199868834e9
Toledo-Luanda <luandatoledoviera@outlook.es>
2023-06-23 23:58:01 -0300

    first commit

---
+++ halloween_nigth/constantes.py
@@ -0,0 +1,6 @@
+ANCHO_VENTANA = 1000
+ALTO_VENTANA = 800
+FPS = 60 #Fotogramas Por Segundo
+
+PATH_IMAGES = "halloween_nigth/recursos"
+
---
+++ halloween_nigth/main.py
@@ -0,0 +1,36 @@
+import pygame, sys
+from constantes import *
+
+
+# SUPERFICIE PRICIPAL (VENTANA)
+ventana_principal = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
+
+pygame.init()
+
+clock = pygame.time.Clock() # Obtenemos el tiempo
+
+img_fondo = pygame.image.load("halloween_nigth/recursos/fondo/1_game_background/1_game_background.png")
+img_fondo = pygame.transform.scale(img_fondo, (ANCHO_VENTANA, ALTO_VENTANA))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.QUIT()
+            sys.exit()
+
+            # player update - verificar como el player interactua con todo el nivel
+            
+
+            # enemigos update
+            # dibujar player
+            # dibujar todo el nivel
+
+    pygame.display.flip()
+    ventana_principal.blit(img_fondo, img_fondo.get_rect())
+
+
+    delta_es = clock.tick(FPS) # Cantidad de tiempo en milisegundos que paso desde que entre a while hasta que salimos
+
+
+
+
